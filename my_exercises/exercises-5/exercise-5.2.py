3919f63f475514314ace6c340a246e51f9b37f9e
= <=>
2021-01-12 22:28:38 +0400

    Add exercise-5.2

--- my_exercises/exercises-5/exercise-5.2.py
+++ my_exercises/exercises-5/exercise-5.2.py
@@ -13,9 +13,17 @@ subnet = int(subnet)
 
 
 bin_subnet =  "1" * subnet + "0" * (32 - subnet)
-split_subnet = bin_subnet.split(maxsplit=4)
+bin_subnet = list(bin_subnet)
 
-print(split_subnet)
+a_str = ('').join(bin_subnet[0:8])
+b_str = ('').join(bin_subnet[8:16])
+c_str = ('').join(bin_subnet[16:24])
+d_str = ('').join(bin_subnet[24:32])
+
+a_int = int(a_str, 2)
+b_int = int(b_str, 2)
+c_int = int(c_str, 2)
+d_int = int(d_str, 2)
 
 network_template = """
                    Network:
@@ -24,7 +32,8 @@ network_template = """
 
                    Mask:
                    /{:<8}
-    
+                   {:<8} {:<8} {:<8} {:<8}
+                   {:08b} {:08b} {:08b} {:08b}
                    """
 
-print(network_template.format(one, two, three, host, one, two, three, host, subnet))
+print(network_template.format(one, two, three, host, one, two, three, host, subnet, a_int, b_int, c_int, d_int, a_int, b_int, c_int, d_int))
