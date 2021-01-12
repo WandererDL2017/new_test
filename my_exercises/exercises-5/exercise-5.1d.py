5e246cc689d29f13e105058bbe088f8018bc6e59
= <=>
2021-01-12 15:59:29 +0400

    ADD NEW FOLDERS and TRANSFER FILES

---
+++ my_exercises/exercises-5/exercise-5.1d.py
@@ -0,0 +1,34 @@
+london_co = {
+    "r1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "4451",
+        "ios": "15.4",
+        "ip": "10.255.0.1"
+    },
+    "r2": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "4451",
+        "ios": "15.4",
+        "ip": "10.255.0.2"
+    },
+    "sw1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "3850",
+        "ios": "3.6.XE",
+        "ip": "10.255.0.101",
+        "vlans": "10,20,30",
+        "routing": True
+    }
+}
+
+divices = input("Введите имя устройства: ")
+parameters = london_co[divices].keys()
+parameters = list(parameters)
+
+parameter = input("Введите имя параметра" + "(" + (', ').join(parameters) + "): " )
+
+print(london_co[divices][parameter])
+
