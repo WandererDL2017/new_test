08e42799d42a3a006b0ecb78db8a1c52055f7488
= <=>
2021-01-13 01:15:34 +0400

    ADD new files exercises 5.2[a,b] and 5.3

---
+++ my_exercises/exercises-5/exercise-5.2a.py
@@ -0,0 +1,39 @@
+network = input('Введите сеть:(ip/mask) ')
+
+network = network.split('.')
+one, two, three, four = network
+four = four.split('/')
+host, subnet = four
+
+one = int(one)
+two = int(two)
+three = int(three)
+host = 0
+subnet = int(subnet)
+
+
+bin_subnet =  "1" * subnet + "0" * (32 - subnet)
+bin_subnet = list(bin_subnet)
+
+a_str = ('').join(bin_subnet[0:8])
+b_str = ('').join(bin_subnet[8:16])
+c_str = ('').join(bin_subnet[16:24])
+d_str = ('').join(bin_subnet[24:32])
+
+a_int = int(a_str, 2)
+b_int = int(b_str, 2)
+c_int = int(c_str, 2)
+d_int = int(d_str, 2)
+
+network_template = """
+                   Network:
+                   {:<8} {:<8} {:<8} {:<8}
+                   {:08b} {:08b} {:08b} {:08b}
+
+                   Mask:
+                   /{:<8}
+                   {:<8} {:<8} {:<8} {:<8}
+                   {:08b} {:08b} {:08b} {:08b}
+                   """
+
+print(network_template.format(one, two, three, host, one, two, three, host, subnet, a_int, b_int, c_int, d_int, a_int, b_int, c_int, d_int))
---
+++ my_exercises/exercises-5/exercise-5.3.py
@@ -0,0 +1,19 @@
+mode = input("Введите режим работы интерфейса (access/trunk): " )
+interface = input("Введите тип и номер интерфейса: ")
+vlans = input("Введите номер влан(ов): ")
+mode_template = {
+        'access_template' : [
+        "switchport mode access", "switchport access vlan {}",
+        "switchport nonegotiate", "spanning-tree portfast",
+        "spanning-tree bpduguard enable"
+        ],
+        'trunk_template' : [
+        "switchport trunk encapsulation dot1q", "switchport mode trunk",
+        "switchport trunk allowed vlan {}"
+        ]
+}
+
+name_mode = '{}_template'.format(mode)
+
+print('interface {}'.format(interface))
+print('\n'.join(mode_template[name_mode]).format(vlans))
