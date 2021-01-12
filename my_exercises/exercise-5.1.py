b9d266332cc3098d418b97b242815b78a6bcd39c
WandererDL2017 <wandererdl@mail.ru>
2021-01-12 14:08:02 +0400

    Add exercise-5.1

--- my_exercises/exercise-5.1.py
+++ my_exercises/exercise-5.1.py
@@ -1,16 +1,28 @@
-ip = "192.168.3.1"
+london_co = {
+    "r1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "4451",
+        "ios": "15.4",
+        "ip": "10.255.0.1"
+    },
+    "r2": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "4451",
+        "ios": "15.4",
+        "ip": "10.255.0.2"
+    },
+    "sw1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "3850",
+        "ios": "3.6.XE",
+        "ip": "10.255.0.101",
+        "vlans": "10,20,30",
+        "routing": True
+    }
+}
 
-ip = ip.split('.')
-one, two, three, four = ip
+name-divices = input("Введите имя устройства: ")
 
-one = int(one)
-two = int(two)
-three = int(three)
-four = int(four)
-
-ip_template = """
-              {:<8} {:<8} {:<8} {:<8}
-              {:08b} {:08b} {:08b} {:08b}
-              """
-
-print(ip_template.format(one, two, three, four, one, two, three, four))
