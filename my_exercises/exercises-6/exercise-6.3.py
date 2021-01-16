eb42bac8299b4d28d6fd3fc6f5a7baadd83cc417
= <=>
2021-01-17 01:57:11 +0400

    Add exercises 6.3 and 6.2

---
+++ my_exercises/exercises-6/exercise-6.3.py
@@ -0,0 +1,36 @@
+access_template = [
+    "switchport mode access",
+    "switchport access vlan",
+    "spanning-tree portfast",
+    "spanning-tree bpduguard enable",
+]
+
+trunk_template = [
+    "switchport trunk encapsulation dot1q",
+    "switchport mode trunk",
+    "switchport trunk allowed vlan",
+]
+
+access = {"0/12": "10", "0/14": "11", "0/16": "17", "0/17": "150"}
+trunk = {"0/1": ["add", "10", "20"], "0/2": ["only", "11", "30"], "0/4": ["del", "17"]}
+
+for intf, vlan in access.items():
+    print("interface FastEthernet" + intf)
+    for command in access_template:
+        if command.endswith("access vlan"):
+            print(f" {command} {vlan}")
+        else:
+            print(f" {command}")
+
+for intf, vlan in trunk.items():
+    print("interface FastEthernet" + intf)
+    for command in trunk_template:
+        if command.endswith("trunk allowed vlan"):
+            if vlan[0] == "add":
+                print(f" {command} {vlan[0]} {(',').join(vlan[1:])}")
+            elif vlan[0] == "only":
+                print(f" {command} {(',').join(vlan[1:])}")
+            elif vlan[0] == "del":
+                print(f" {command} {'remove'} {(',').join(vlan[1:])}")
+        else:
+            print(f" {command}")
--- my_exercises/exercises-6/new.py
+++
@@ -1,9 +0,0 @@
-i= ['8','8','7','7']
-
-for j in i:
-    if int(j) in range(0, 8):
-        print('in range')
-        break
-    else:
-        print('not in range')
-        break
